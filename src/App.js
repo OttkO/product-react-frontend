ba1aa957868ea971fe0b354c8950715257bcfbcb
Adrian Yankov <adrian.yankov@tno.nl>
2020-11-05 16:49:47 +0100

    making small progress

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import HeaderComponent from './components/HeaderComponent';
 import ProductComponent from './components/ProductComponent';
 
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import CreateProductComponent from './components/CreateProductComponent';
 
 function App() {
   return (
@@ -13,8 +14,9 @@ function App() {
             <HeaderComponent/>
               <div className="container">
                 <Switch> 
-                    <Route path = "/" component = {ProductComponent}></Route>
+                    <Route path = "/" exact component = {ProductComponent}></Route>
                     <Route path = "/products" component = {ProductComponent}></Route>
+                    <Route path = "/add-product" component = {CreateProductComponent} ></Route>
                     <ProductComponent/>
                 </Switch>
               </div>
---
+++ src/components/CreateProductComponent.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class CreateProductComponent extends Component {
+    render() {
+        return (
+            <div>
+                <h1>THIS IS THE ADD PRODUCT PAGE</h1>
+            </div>
+        );
+    }
+}
+
+export default CreateProductComponent;
