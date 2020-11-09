c884360701b9b4dd8f852f9089e8dd07f5d3c918
Adrian Yankov <adrian.yankov@tno.nl>
2020-11-09 11:37:25 +0100

    prepared the view product component with routing

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import ProductComponent from './components/ProductComponent';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import CreateProductComponent from './components/CreateProductComponent';
 import UpdateProductComponent from './components/UpdateProductComponent';
+import ViewProductComponent from './components/ViewProductComponent';
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
                     <Route path = "/products" component = {ProductComponent}></Route>
                     <Route path = "/add-product" component = {CreateProductComponent} ></Route>
                     <Route path = "/update-product/:id" component = {UpdateProductComponent} ></Route>
+                    <Route path = "/view-product/:id" component = {ViewProductComponent} ></Route>
                     <ProductComponent/>
                 </Switch>
               </div>
---
+++ src/components/ViewProductComponent.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class ViewProductComponent extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+
+        }
+    }
+    render() {
+        return (
+            <div>
+                <h2>View Product Page</h2>
+            </div>
+        );
+    }
+}
+
+export default ViewProductComponent;
+
